25c17eaf033908a33c3f0e23426cba7d04b9c137
Samwise12 <sfelder1@bingahmton.edu>
2016-06-18 09:31:22 -0400

    a

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/:inputDate?', function(req,res){
 	
 })
 
-var port = Number(process.env.Port || 3000);
+var port = Number(process.env.PORT || 5000);
 app.listen(port, function(){
 	console.log('app running');
 });
